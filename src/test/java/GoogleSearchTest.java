3875c0ac571848e2e641c7330d285d625a3e146e
borisovaanna <86157809+borisovaanna@users.noreply.github.com>
2021-08-20 21:48:07 +0300

    Update GoogleSearchTest.java
    Task_5

--- src/test/java/GoogleSearchTest.java
+++ src/test/java/GoogleSearchTest.java
@@ -2,6 +2,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.opera.OperaDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -12,8 +13,11 @@ public class GoogleSearchTest {
     public void test1(){
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.google.by");
-        driver.findElement(By.name("q")).sendKeys("Hello world!", Keys.ENTER);
-        Assert.assertEquals(driver.findElement(By.xpath("(//h3)[1]")).getText(), "Hello, world! Карта для тех, кто летает | МТБанк");
+        driver.findElement(By.name("q")).sendKeys("Привет мир", Keys.ENTER);
+        Actions actions = new Actions(driver);
+        actions.moveToElement(driver.findElement(By.xpath("(//h3)[12]"))).perform();
+        driver.findElement(By.xpath("(//h3)[12]")).click();
+        Assert.assertEquals(driver.findElement(By.id("firstHeading")).getText(), "Hello, world!");
         driver.quit();
     }
 
@@ -23,6 +27,10 @@ public class GoogleSearchTest {
         driver.get("https://www.google.by");
         driver.findElement(By.name("q")).sendKeys("//", Keys.ENTER);
         Assert.assertEquals(driver.findElement(By.xpath("(//p)[1]")).getText(), "По запросу // ничего не найдено. ");
+        Assert.assertEquals(driver.findElement(By.xpath("(//p)[2]")).getText(), "Рекомендации:");
+        Assert.assertEquals(driver.findElement(By.xpath("(//ul/li)[1]")).getText(), "Убедитесь, что все слова написаны без ошибок.");
+        Assert.assertEquals(driver.findElement(By.xpath("(//ul/li)[2]")).getText(), "Попробуйте использовать другие ключевые слова.");
+        Assert.assertEquals(driver.findElement(By.xpath("(//ul/li)[3]")).getText(), "Попробуйте использовать более популярные ключевые слова.");
         driver.quit();
     }
 
